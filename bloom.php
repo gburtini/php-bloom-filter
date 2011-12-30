386f869eeeacdd2adfd00889fcc154375da2840a
Giuseppe Burtini <joe@truephp.com>
2011-12-30 15:13:15 -0800

    Finished bloom and editable bloom filters. Neat stuff. Editable bloom filter works by storing a deleted filter (doubles the size), but can be consolidated down for speed reasons if needed.

--- bloom.php
+++ bloom.php
@@ -7,34 +7,92 @@ class Bloom {
    protected $hashCount;
    protected $count;
 
+   function constructorElements($elementsCount) {
+      $predictSize = $this->desirableSize($elementsCount);
+      $predictHashes = $this->desirableHashCount($predictSize, $elementsCount);
+      return $this->__construct($predictSize, $predictHashes);
+   }
 
-   function __construct($size, $hashCount=3, $hashes=null) {
-      if(function_exists("murmurhash3"))
-         $this->hashFunction = "murmurhash3";
+   function constructorSize($size, $hashCount, $hash=null) {
+      if($hash !== null) {
+         $this->hashFunction = $hash;
+      } else {
+         if(function_exists("murmurhash3"))
+            $this->hashFunction = "murmurhash3";
 
-      if(function_exists("murmurhash"))
-         $this->hashFunction = "murmurhash";
-
-      if($hashes !== null) {
-         $this->hashFunction = $hashes;
+         if(function_exists("murmurhash"))
+            $this->hashFunction = "murmurhash";
       }
 
       $this->bitArray = new SplFixedArray($size);
       $this->hashCount = $hashCount;
    }
 
+   function __construct() {   // should maybe use __call to do this overloading.
+      $count = func_num_args();
+      if($count == 0)
+         return trigger_error("Not enough arguments passed to constructor for Bloom.");
+
+      if($count == 1)
+         return $this->constructorElements (func_get_arg(0));
+
+      $hash = $size = $hashCount = null;
+
+      $size = func_get_arg(0);
+      $hashCount = func_get_arg(1);
+      if($count == 3)
+         $hash = func_get_arg(2);
+
+      return $this->constructorSize($size, $hashCount, $hash);
+   }
+
+   /*
+    * desirable* static functions care of http://0pointer.de/blog/projects/bloom.html
+    *
+    * these compute estimates of the desirable size and hash count of your bloom filter.
+    */
+
+   //m = -n*ln(p)/(ln(2)^2) // probability = desired probability of a false positive.
+   public static function desirableSize($elements, $probability=0.05) {
+      return (-$elements * log($probability) / (pow(log(2), 2)));
+   }
+   //k = 0.7*m/n
+   public static function desirableHashCount($size, $elements) {
+      return ceil(0.7 * ($size / $elements));
+   }
+
    public function add($key) {
       if(is_array($key))
       {
          foreach($key as $k) { $this->add($k); }
-         return;
+         return true;
       }
 
       $this->count++;
 
       foreach($this->bits($key) as $bit)
          $this->bitArray[$bit] = 1;
+
+      return true;
    }
+
+   public function has($key) {
+      if(is_array($key)) {
+         foreach($key as $k) {
+            if(!$this->has($k))
+               return false;
+         }
+         return true;
+      }
+
+      foreach($this->bits($key) as $bit)
+      {
+         if(!$this->bitArray[$bit])
+            return false;
+      }
+      return true; // possibly true.
+   }
+
 
    private function bits($key) {
       /*
@@ -53,26 +111,18 @@ class Bloom {
       return $return;
    }
 
-   public function has($key) {
-      foreach($this->bits($key) as $bit)
-      {
-         if(!$this->bitArray[$bit])
-            return false;
-      }
-      return true; // possibly true.
-   }
-
    private function computeHash($string) {
-      if(!is_array($this->hashFunction))
-      {
-         $function = $this->hashFunction;
-         return $function($string);
-      }
-
-      $currentFunction = $this->hashFunction[$this->hashIndex++];
-      $this->hashIndex = $this->hashIndex % count($this->hashFunction);
-      return $currentFunction($string);
+      $function = $this->hashFunction;
+      return $function($string);
    }
 
+   public function __toString() {
+      $string = "Size " . $this->bitArray->getSize() . " bloom filter currently holding " . $count . " elements.\n";
+      for($i=0; $i<$this->bitArray->getSize(); $i++) {
+         $string .= ($this->bitArray[$i] == 1) ? "1" : "0";
+      }
+      $string .= "\n";
+      return $string;
+   }
 }
 ?>
---
+++ editablebloom.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * A bloom filter that can have items removed. Remove works by storing a second
+ * bloom filter with the deleted items in it. You can consolidate everything down
+ * to the single bloom filter by calling EditableBloom::consolidate (which wipes
+ * the deleted filter and reconstructs the new bloom filter from scratch).
+ *
+ * Also adds a confirm option to the has() function, which checks the stored list
+ * for the value directly. This is useful if most of your requests are false, as
+ * the bloom filter should still be faster than the in_array check.
+ */
+class EditableBloom extends Bloom {
+   private $data;
+   private $deleted;
+
+   public function add($key) {
+      if(is_array($key)) {
+         foreach($key as $k) {
+            $this->add($k);
+         }
+         return true;
+      }
+
+      $this->data[] = $key;
+      parent::add($key);
+   }
+
+   public function remove($key, $consolidate=false) {
+      if(is_array($key)) {
+         foreach($key as $k) {
+            $this->remove($k);
+         }
+         return true;
+      }
+
+      if(false !== ($index = array_search($key, $this->data)))
+         unset($this->data[$index]);
+      else
+         return false;
+
+      if($consolidate) {
+         $this->consolidate();
+      } else {
+         $class = __CLASS__;
+         if(!is_object($this->deleted))
+            $this->deleted = new $class($this->bitArray->getSize(), $this->hashCount);
+
+         return $this->deleted->add($key);
+      }
+   }
+
+   private function confirm($key) {
+      return in_array($key, $this->data);
+   }
+   public function has($key, $confirm=false) {
+      if(parent::has($key)) {
+         $stillExists = !$this->deleted->has($key);
+         if(!$confirm)
+            return $stillExists;
+
+         if($stillExists) {
+            return $this->confirm($key);
+         } else {
+            return $this->deleted->confirm($key);
+         }
+      } else {
+         return false;
+      }
+   }
+
+   public function rebuild() { $this->consolidate(); }
+   public function consolidate() {
+      for($i=0;$i<$this->bitArray->getSize();$i++) {
+         $this->bitArray[$i] = 0;
+      }
+
+      $data = $this->data;
+      $this->data = array();
+      $this->deleted = null;
+
+      return $this->add($data);
+   }
+}
+
+?>
